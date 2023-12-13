6aa5a5990dfa8d21572785e4af0f4e7e4b9ba687
andres <148400031+andres-rivera-05@users.noreply.github.com>
2023-12-13 17:53:43 -0600

    andres

---
+++ ViewModels/SumaMvvm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+     public partial class SumaMvvm : ObservableObject
+    {
+        [ObservableProperty]
+        public double valor1;
+
+        [ObservableProperty]
+        public double valor2;
+
+        [ObservableProperty]
+        public double resultado;
+
+
+        [RelayCommand]
+        public void Sumar() 
+        {
+            Resultado = Valor1 + Valor2;         
+        }
+    }
+}
---
+++ Views/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace SumaAppMvvm.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
